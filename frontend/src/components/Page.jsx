064d68438efa828a2d8f0fc0ba7620e31f1780cc
Brice Frisco <bricemfrisco@gmail.com>
2025-12-12 13:49:25 -0600

    add metrics placeholders

--- frontend/src/components/Page.jsx
+++ frontend/src/components/Page.jsx
@@ -1,12 +1,15 @@
 import Header from './Header';
 
-const Page = ({ children, title }) => {
+const Page = ({ children, title, description }) => {
   return (
     <div className="bg-black text-white min-h-screen flex flex-col">
       <Header />
       <div className="max-w-7xl mx-auto border-l border-r border-white/15 flex-1 w-full p-4">
         <h1 className="text-3xl font-bold">{title}</h1>
-        <div className="mt-200">{children}</div>
+        {description && (
+          <p className="mt-4 text-lg text-gray-300">{description}</p>
+        )}
+        <div className="mt-8">{children}</div>
       </div>
     </div>
   );
