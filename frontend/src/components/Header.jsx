2eb4a836c7dcd29b1cfb750bb59af01304735bf9
Brice Frisco <bricemfrisco@gmail.com>
2025-12-12 10:32:47 -0600

    added search icon

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,17 @@ const Header = () => {
       <div className="max-w-7xl mx-auto border-l border-r border-white/15 flex items-center">
         <div className="py-3 px-5 text-lg font-bold">AlbionStats</div>
         <div className="flex-1 flex justify-center">
-          <Input className="bg-black-900 border border-white/15 rounded-full px-5 py-1 min-w-96 bg-zinc-900" />
+          <div className="relative">
+            <Input
+              placeholder="Search players..."
+              className="bg-zinc-900 border border-white/15 rounded-full pl-10 pr-5 py-1 min-w-96 focus:border-blue-400 focus:outline-none placeholder-gray-400"
+            />
+            <img
+              src="/search.svg"
+              alt="Search"
+              className="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4 brightness-0 invert"
+            />
+          </div>
         </div>
         <a
           href="https://github.com/bricefrisco/AlbionStats"
