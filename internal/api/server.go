c7a6eff60ff883d88f4120a34875911ea3423ca9
Brice <bricemfrisco@gmail.com>
2026-01-11 19:11:13 -0600

    fix route conflict - /metrics/pvp/:server/:playerId

--- internal/api/server.go
+++ internal/api/server.go
@@ -33,8 +33,9 @@ func NewServer(cfg Config) *Server {
 func (s *Server) setupRoutes() {
 	v1 := s.router.Group("/api")
 	v1.GET("/metrics/:metricId", s.metrics)
+	v1.GET("/metrics/pvp/:server/:playerId", s.playerPvp)
+
 	v1.GET("/players/:server/:name", s.player)
-	v1.GET("/players/:server/:playerId/pvp", s.playerPvp)
 	// v1.GET("/players/:server/:name/pve", s.playerPve)
 	v1.GET("/admin", s.admin)
 	v1.GET("/search/:server/:query", s.search)
