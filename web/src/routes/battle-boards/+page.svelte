bb7a92df6e4da75bd2809e6c1380775ffc41ed65
Brice <bricemfrisco@gmail.com>
2026-01-19 13:30:07 -0600

    add battle-boards base page

---
+++ web/src/routes/battle-boards/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Page from '$components/Page.svelte';
+	import PageHeader from '$components/PageHeader.svelte';
+</script>
+
+<Page>
+    <PageHeader title="Battle Boards" />
+</Page>
