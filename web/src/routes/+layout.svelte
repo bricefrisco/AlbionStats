96df4f42549c70510b37ba3183590a0f714f6ae7
Brice <bricemfrisco@gmail.com>
2026-01-23 11:24:24 -0600

    enhancement: added nprogress

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,8 +1,32 @@
 <script>
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { afterNavigate, beforeNavigate } from '$app/navigation';
+	import { onMount } from 'svelte';
+	import NProgress from 'nprogress';
 
 	let { children } = $props();
+	let startTimer;
+
+	onMount(() => {
+		NProgress.configure({
+			minimum: 0.12,
+			trickleSpeed: 120,
+			showSpinner: false
+		});
+	});
+
+	beforeNavigate(() => {
+		clearTimeout(startTimer);
+		startTimer = setTimeout(() => {
+			NProgress.start();
+		}, 120);
+	});
+
+	afterNavigate(() => {
+		clearTimeout(startTimer);
+		NProgress.done();
+	});
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
